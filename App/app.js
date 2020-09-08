da789c19d7252b103bca6bd1e775729c105dbade
Pravpro <praviradlakha@hotmail.com>
2020-09-08 10:27:40 +0000

    Created login functionality; Created movies page with the ability to pull from api and save nominations to users.

--- App/app.js
+++ App/app.js
@@ -1,22 +1,66 @@
 const express = require("express"),
 	  app = express(),
-	  mongoose = require("mongoose");
+	  mongoose = require("mongoose"),
+	  bodyParser = require('body-parser');
 
 mongoose.connect("mongodb://localhost:27017/shoppies", { useNewUrlParser: true, useUnifiedTopology: true } )
 .then(() => console.log("Connected to shoppies DB!"))
 .catch(err => console.log(err));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// User - email, pasword, nominations
+const userSchema = new mongoose.Schema({
+	email: String,
+	password: String,
+	nominations: { type: [String], default: [] },
+	isDone: { type: Boolean, default: false }
+});
 
-// 
+const User = mongoose.model("User", userSchema);
+
+// User.create({
+// 	email: "luffy@gmail.com",
+// 	password: "gumgumpistol"
+// });
+
+// Page will show login form, and Register button
 app.get("/", (req, res) => {
-	res.send("Home");
-})
+	res.redirect("/login");
+});
 
-// Look up user and redirect to movies page
+// Show login page
 app.get("/login", (req, res) => {
-	res.send("Login!");
+	res.render("login", { attempted: false });
+});
+
+app.get("/login/incorrect", (req, res) => {
+	res.render("/login", {attempted: true})
 })
 
-app.
+// Looks up user and if exists then displays movies page
+app.get("/movies", (req, res) => {
+	User.findOne(req.body.user)
+	.then(user => res.render("movies", {user: user}))
+	.catch(err => {res.send("Something went wrong!")});
+})
+
+app.post("/nominate", (req, res) => {
+	User.findOne(req.body.userId)
+	.then( user => {
+		if(user.nominations.length >= 5){
+			user.isDone = true;
+		}
+		else {
+			user.nominations.push(req.body.movieId);
+		}
+		let nomsLeft = 5 - user.nominations.length;
+		res.send({nomsLeft : nomsLeft.toString()});
+	})
+})
 
 app.listen("3000", () => console.log("The Shoppies server started on port 3000!"));
+
+
+
---
+++ App/public/scripts/movies.js
@@ -0,0 +1,73 @@
+function searchAndDisplay(query) {
+	axios.get("https://www.omdbapi.com/?s=" + query + "&type=movie&apikey=746de64b")
+	.then(response => {
+		console.log(JSON.stringify(response.data));
+		let resultsHtml = "";
+		if(response.data["Response"] == "True"){
+			resultsHtml += '<h4>Search Results for "' + $("#search").val() + '"</h4>';
+			response.data["Search"].forEach( movie => {
+				resultsHtml += '<div class="card mb-3">' +
+						'<div class="row no-gutters">' +
+							'<div class="col-md-4">' +
+								'<img src="' + movie['Poster'] + '" class="card-img" alt="' + movie['Title'] + ' Poster">' +
+							'</div>' +
+							'<div class="col-md-8">' +
+								'<div class="card-body">' +
+									'<h5 class="card-title">' + movie['Title'] + '</h5>' +
+									'<p class="card-text">' + movie['Year'] + '</p>' +
+									'<button class="btn btn-outline-success nominate">Nominate</button>' +
+									'<input type="hidden" value="' + movie["imdbID"] + '">' +
+								'</div>' +
+							'</div>' +
+						'</div>' +
+					'</div>';
+			});
+		} else {
+			console.log("Search was invalid!");
+			resultsHtml += '<div class="alert alert-info" role="alert">' + 
+				'Search did not return any results. Please try again.' + 
+				'</div>';
+		}
+		$("#search-results").html(resultsHtml);
+	})
+	.catch( error => console.log(err));
+}
+
+
+function nominate(id){
+	axios.post("/nominate", {
+		userId: $("#user-id").val(),
+		movieId: id
+	})
+	.then(response => {
+		if(response.status == 200){
+			let alertHtml = '<div class="alert alert-success alert-dismissible fade show" role="alert">' +
+				  'Movie successfuly added to nomination list! You have ' + response.data.nomsLeft + ' nominations left.' +
+				  '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+					'<span aria-hidden="true">&times;</span>' +
+				  '</button>' +
+				'</div>';
+			$(".container").first().prepend(alertHtml);
+		}
+	})
+	.catch(error => console.log(error));
+}
+
+$("#search-btn").on("click", () => {
+	searchAndDisplay($("#search").val());
+});
+
+
+$(document).on("click", ".nominate", function() {
+	let imdbID = $(this).next().val();
+	console.log(imdbID);
+	nominate(imdbID);
+})
+
+$("#search").on("keypress", e => {
+	if(e.which == 13) {
+		searchAndDisplay($("#search").val());
+	}
+})
+
+
